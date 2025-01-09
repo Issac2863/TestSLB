5041fb181018323695f4dc89aff41c7b75b65ba1
newolf2863 <issacdelacadena@gmail.com>
2025-01-09 08:59:42 -0500

    Creation Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './componetMain/main/main.component';
+import { FormComponent } from './componetForm/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'form', component: FormComponent }
+];
--- src/app/componetForm/form/form.component.ts
+++ src/app/componetForm/form/form.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
